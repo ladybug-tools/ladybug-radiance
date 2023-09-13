0f9a4b3442dbf2269dfa5e7e35befdd046925b47
Mikkel Pedersen <mkklpdrsn@gmail.com>
2023-09-13 19:23:39 +0200

    fix(intersection): Add binary reader functions
    This removes honeybee-radiance-postprocess as a dependency.

--- ladybug_radiance/intersection.py
+++ ladybug_radiance/intersection.py
@@ -4,12 +4,12 @@ import os
 import subprocess
 import tempfile
 import math
+from typing import Tuple
 import numpy as np
 
 from ladybug_geometry.geometry3d import Face3D, Mesh3D
 from ladybug.futil import write_to_file_by_name
 from ladybug.viewsphere import view_sphere
-from honeybee_radiance_postprocess.reader import binary_to_array
 
 from .config import folders
 
@@ -190,3 +190,87 @@ def sky_intersection_matrix(sky_matrix, points, normals, context_geometry,
     return intersection_matrix(
         vectors, points, normals, context_geometry,
         offset_distance, numericalize, sim_folder)
+
+
+def binary_to_array(
+        binary_file: str, nrows: int = None, ncols: int = None,
+        ncomp: int = None, line_count: int = 0) -> np.ndarray:
+    """Read a Radiance binary file as a NumPy array.
+
+    Args:
+        binary_file: Path to binary Radiance file.
+        nrows: Number of rows in the Radiance file.
+        ncols: Number of columns in the Radiance file.
+        ncomp: Number of components of each element in the Radiance file.
+        line_count: Number of lines to skip in the input file. Usually used to
+            skip the header.
+
+    Returns:
+        A NumPy array.
+    """
+    with open(binary_file, 'rb') as reader:
+        if (nrows or ncols or ncomp) is None:
+            # get nrows, ncols and header line count
+            nrows, ncols, ncomp, line_count = binary_mtx_dimension(binary_file)
+        # skip first n lines from reader
+        for i in range(line_count):
+            reader.readline()
+
+        array = np.fromfile(reader, dtype=np.float32)
+        if ncomp != 1:
+            array = array.reshape(nrows, ncols, ncomp)
+        else:
+            array = array.reshape(nrows, ncols)
+
+    return array
+
+
+def binary_mtx_dimension(filepath: str) -> Tuple[int, int, int, int]:
+    """Return binary Radiance matrix dimensions if it exists.
+
+    This function returns NROWS, NCOLS, NCOMP and number of header lines including the
+    empty line after last header line.
+
+    Args:
+        filepath: Full path to Radiance file.
+
+    Returns:
+        nrows, ncols, ncomp, line_count
+    """
+    try:
+        inf = open(filepath, 'rb', encoding='utf-8')
+    except Exception:
+        inf = open(filepath, 'rb')
+    try:
+        first_line = next(inf).rstrip().decode('utf-8')
+        if first_line[:10] != '#?RADIANCE':
+            error_message = (
+                f'File with Radiance header must start with #?RADIANCE not '
+                f'{first_line}.'
+            )
+            raise ValueError(error_message)
+
+        header_lines = [first_line]
+        nrows = ncols = ncomp = None
+        for line in inf:
+            line = line.rstrip().decode('utf-8')
+            header_lines.append(line)
+            if line[:6] == 'NROWS=':
+                nrows = int(line.split('=')[-1])
+            if line[:6] == 'NCOLS=':
+                ncols = int(line.split('=')[-1])
+            if line[:6] == 'NCOMP=':
+                ncomp = int(line.split('=')[-1])
+            if line[:7] == 'FORMAT=':
+                break
+
+        if not nrows or not ncols:
+            error_message = (
+                f'NROWS or NCOLS was not found in the Radiance header. NROWS '
+                f'is {nrows} and NCOLS is {ncols}. The header must have both '
+                f'elements.'
+            )
+            raise ValueError(error_message)
+        return nrows, ncols, ncomp, len(header_lines) + 1
+    finally:
+        inf.close()
